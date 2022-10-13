cd9f1db164f5abda13d696be06f8761327e8ea24
Pawan Kartik <85254170+pawan-deepsource@users.noreply.github.com>
2022-10-13 18:22:15 +0530

    Add missing interpolation

--- GZCTF/Services/K8sService.cs
+++ GZCTF/Services/K8sService.cs
@@ -260,8 +260,13 @@ public class K8sService : IContainerService
         if (withAuth && registry is not null)
         {
             var auth = Codec.Base64.Encode($"{registry.UserName}:{registry.Password}");
-            var dockerjson = $"{{\"auths\":{{\"{registry.ServerAddress}\":{{\"auth\":\"{auth}\"," +
-                $"\"username\":\"{registry.UserName}\",\"password\":\"{registry.Password}\"}}}}}}";
+            var dockerjson = Codec.Base64.EncodeToBytes(
+                $"{{\"auths\":" +
+                    $"{{\"{registry.ServerAddress}\":" +
+                        $"{{\"auth\":\"{auth}\"," +
+                        $"\"username\":\"{registry.UserName}\"," +
+                        $"\"password\":\"{registry.Password}\"" +
+                $"}}}}}}");
             var dockerjsonBytes = Codec.Base64.EncodeToBytes(dockerjson);
             var secret = new V1Secret()
             {
